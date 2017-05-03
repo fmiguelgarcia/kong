5235fb0bb171f3bf21e519dadde6f8263f8f3040
thefosk <marco@mashape.com>
2017-05-03 11:46:35 -0700

    fix(logging) include 'balancer_by_lua' latency in Kong latency

--- kong/kong.lua
+++ kong/kong.lua
@@ -225,6 +225,8 @@ function Kong.ssl_certificate()
 end
 
 function Kong.balancer()
+  core.balancer.before()
+
   local addr = ngx.ctx.balancer_address
   local tries = addr.tries
 
@@ -275,6 +277,8 @@ function Kong.balancer()
   if not ok then
     ngx.log(ngx.ERR, "could not set upstream timeouts: ", err)
   end
+
+  core.balancer.after()
 end
 
 function Kong.rewrite()
